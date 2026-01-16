e77aa1a01833708f11c21f8f08d1cd20cbeb2eb5
Dallas <119005260+Manzidallas@users.noreply.github.com>
2026-01-16 10:53:19 +0200

    Create Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,8 @@
+// Button.tsx
+import React from 'react';
+
+const Button: React.FC<{ label: string; onClick: () => void }> = ({ label, onClick }) => {
+    return <button onClick={onClick}>{label}</button>;
+};
+
+export default Button;

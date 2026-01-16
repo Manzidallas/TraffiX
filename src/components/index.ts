596d7d21aa33e8d6a5951ebc512040be82464b09
Dallas <119005260+Manzidallas@users.noreply.github.com>
2026-01-16 10:54:27 +0200

    Create src/components/index.ts for component exports

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+// Export components from this file
+
+export { default as ComponentA } from './ComponentA';
+export { default as ComponentB } from './ComponentB';
+export { default as ComponentC } from './ComponentC';

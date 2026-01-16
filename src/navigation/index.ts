d13365170f917e270d15b77e275a587968e3dc12
Dallas <119005260+Manzidallas@users.noreply.github.com>
2026-01-16 10:54:59 +0200

    Create src/navigation/index.ts for navigation exports

---
+++ src/navigation/index.ts
@@ -0,0 +1,6 @@
+// Export navigation components
+
+export { default as Navigation } from './Navigation';
+export { default as Navbar } from './Navbar';
+export { default as Sidebar } from './Sidebar';
+// Add more exports as needed...
